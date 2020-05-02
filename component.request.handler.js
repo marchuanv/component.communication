c229e282cd4bafe9fbfc93d6633273b2baf01414
marchuanv <marchuanv@gmail.com>
2020-05-02 22:27:48 +0200

    init

--- component.request.handler.js
+++ component.request.handler.js
@@ -3,6 +3,15 @@ const logging = require("logging");
 const utils = require("utils");
 const componentRequestHandlerSecure = require("component.request.handler.secure");
 module.exports = { 
-    callback: ()=>{}, 
+    callback: ({ host, port, path, headers, data })=>{
+        return {
+            statusCode: 200,
+            statusMessage: "Success",
+            headers: {
+
+            },
+            data: "Hello World"
+        }
+    }, 
     register: ()=>{}
 };
