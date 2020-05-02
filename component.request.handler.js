64ae758c6746a90e57a03a88575d57176b1abbd5
marchuanv <marchuanv@gmail.com>
2020-05-02 22:04:16 +0200

    init

---
+++ component.request.handler.js
@@ -0,0 +1,8 @@
+const http = require("http");
+const logging = require("logging");
+const utils = require("utils");
+const componentRequestHandlerSecure = require("component.request.handler.secure");
+module.exports = { 
+    callback: ()=>{}, 
+    register: ()=>{}
+};
---
+++ test.js
@@ -0,0 +1,8 @@
+const component = require("./component.request.handler.secure.js");
+const logging = require("logging");
+logging.config(["Component Client","Component Server","Component Secure Client","Component Secure Server"]);
+(async()=>{
+       
+})().catch((err)=>{
+    console.error(err);
+});
