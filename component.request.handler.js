7eca7367439cba4025da2054389eb6ab9706a749
marchuanv <marchuanv@gmail.com>
2020-05-02 22:42:04 +0200

    init

--- component.request.handler.js
+++ component.request.handler.js
@@ -4,14 +4,29 @@ const utils = require("utils");
 const componentRequestHandlerSecure = require("component.request.handler.secure");
 module.exports = { 
     callback: async ({ host, port, path, headers, data })=>{
-        return {
-            statusCode: 200,
-            statusMessage: "Success",
-            headers: {
 
-            },
-            data: "Hello World"
-        }
+       return await componentRequestHandlerSecure.callback({ host, port, path, requestHeaders: headers, requestData: data });
+
+        // callback: ({data, fromhost, fromport }) => {
+        //     return new Promise(async (resolve) => {
+        //         let tryCount = 0;
+        //         const id = setInterval(() => {
+        //             tryCount = tryCount + 1;
+        //             const results = await requestHandlers.getResults()
+        //             if (){
+        //                 clearInterval(id);
+        //                 results.statusCode = 200;
+        //                 resolve(results);
+        //             } else if (tryCount === 3) {
+        //                 clearInterval(id);
+        //                 logging.write("Server Request",`deferring ${requestUrl} request`);
+        //                 resolve({ data: "Request Deferred", contentType: "text/plain", statusCode: 202 });
+        //                 await requestHandlers.defer({ publicHost, publicPort, privateHost, privatePort, path: request.url }, { data, fromhost, fromport });
+        //             }
+        //         }, 1000);
+                
+        //     });
+        // }
     }, 
     register: ()=>{}
 };
