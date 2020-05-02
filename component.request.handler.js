ef72d3debb6a8388833e2282668a15068a186ae1
marchuanv <marchuanv@gmail.com>
2020-05-02 22:31:06 +0200

    init

--- component.request.handler.js
+++ component.request.handler.js
@@ -3,7 +3,7 @@ const logging = require("logging");
 const utils = require("utils");
 const componentRequestHandlerSecure = require("component.request.handler.secure");
 module.exports = { 
-    callback: ({ host, port, path, headers, data })=>{
+    callback: async ({ host, port, path, headers, data })=>{
         return {
             statusCode: 200,
             statusMessage: "Success",
