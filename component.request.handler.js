2cfcdbe72d320c1a790919fe342582f6f7ef8660
marchuanv <marchuanv@gmail.com>
2020-06-09 22:23:31 +0200

    refactor

--- component.request.handler.js
+++ component.request.handler.js
@@ -2,39 +2,51 @@ const http = require("http");
 const delegate = require("component.delegate");
 const logging = require("logging");
 logging.config.add("Request Handler");
-
-module.exports = { handle: ({ callingModuleName, port }) => {
-    const server = http.createServer();
-    server.on("request", (request, response)=>{
-        let body = '';
-        request.on('data', chunk => {
-            body += chunk.toString();
-        });
-        request.on('end', () => {
-            logging.write("Request Handler",`received request for ${request.url}`);
-            const defaultHeaders = {
-                "Access-Control-Allow-Origin": "*",
-                "Access-Control-Expose-Headers": "*",
-                "Access-Control-Allow-Headers": "*",
-                "Content-Type": "text/plain"
-            };
-            const isPreflight = request.headers["access-control-request-headers"] !== undefined;
-            if(isPreflight){
-                return response.writeHead( 200, "Success", defaultHeaders ).end("");
-            }
-            try {
-                let results = delegate.call(callingModuleName, {  path: request.url, headers: request.headers, data: body });
-                if (results && results.then){
-                    results = await results.catch((error)=>{
-                        logging.write("Request Handler"," ", error.toString());
-                        response.writeHead( 500, "Internal Server Error").end();
-                    });
+module.exports = {
+    instances: [],
+    handle: ({ callingModule, port }) => {
+        let instance = module.exports.instances.find(s => s.port === port);
+        if (!instance){
+            instance= http.createServer();
+            module.exports.instances.push(instance);
+        }
+        instance.removeAllListeners("request");
+        instance.on("request", (request, response)=>{
+            let body = '';
+            request.on('data', chunk => {
+                body += chunk.toString();
+            });
+            request.on('end', async () => {
+                logging.write("Request Handler",`received request for ${request.url}`);
+                const defaultHeaders = {
+                    "Access-Control-Allow-Origin": "*",
+                    "Access-Control-Expose-Headers": "*",
+                    "Access-Control-Allow-Headers": "*",
+                    "Content-Type": "text/plain"
+                };
+                const isPreflight = request.headers["access-control-request-headers"] !== undefined;
+                if(isPreflight){
+                    return response.writeHead( 200, "Success", defaultHeaders ).end("");
                 }
-                response.writeHead( results.statusCode, results.statusMessage, results.headers).end(results.data);
-            } catch {
-                response.writeHead( 500, "Internal Server Error").end();
-            }
+                try {
+                    let results = delegate.call(callingModule, { path: request.url, headers: request.headers, data: body });
+                    if (results && results.then){
+                        results = await results.catch((error)=>{
+                            logging.write("Request Handler"," ", error.toString());
+                            response.writeHead( 500, "Internal Server Error").end();
+                        });
+                    }
+                    response.writeHead( results.statusCode, results.statusMessage, results.headers).end(results.data);
+                } catch {
+                    response.writeHead( 500, "Internal Server Error").end();
+                }
+            });
         });
-    });
-    server.listen(port);
-}};
+        if (instance.listening === false){
+            instance.listen(port);
+            logging.write("Request Handler", `listening on port ${port}`);
+        }
+        const count = instance.listeners("request").length;
+        logging.write("Request Handler",`http request event count: ${count}`);
+    }
+};
--- test.js
+++ test.js
@@ -1,15 +1,11 @@
 const requestHandler = require("./component.request.handler.js");
-const logging = require("logging");
-logging.config(["Request Handler"]);
-const handle = async () => {
-    (await requestHandler.port({port: 3000})).handle(async({ path, headers, data }) => {
-        handle();
-        console.log("TEST: received request: ", { path, headers, data });
+const delegate = require("component.delegate");
+(async()=>{ 
+    const callingModule = "component.request.handler.route";
+    delegate.register(callingModule, (callback) => {
         return { statusCode: 200, statusMessage: "Success", headers: {}, data: null };
     });
-};
-(async()=>{ 
-    await handle();
+    await requestHandler.handle({ callingModule, port: 3000 });
 })().catch((err)=>{
     console.error(err);
 });
