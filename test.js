f1075f932f496a93565d9e46c9d9073720dc5f8c
marchuanv <marchuanv@gmail.com>
2021-03-10 01:24:56 +0200

    fix

--- test.js
+++ test.js
@@ -1,13 +1,13 @@
 (async()=>{ 
     
     const requestHandler = require("./component.request.handler.js");
-    const component = require("component");
+    const com = require("component");
     const delegate = require("component.delegate");
 
-    delegate.register("component.request.handler.route",3000,() => {
+    delegate.register({ context: "component.request.handler.route", name: 3000 },() => {
         throw new Error("Error on port 3000");
     });
-    delegate.register("component.request.handler.route",4000,() => {
+    delegate.register({ context: "component.request.handler.route", name: 4000 },() => {
         return {
             headers: { "content-type":"text/plain" },
             data: "route 02",
@@ -15,7 +15,7 @@
             statusCode: 200
         };
     });
-    delegate.register("component.request.handler.route",5000,() => {
+    delegate.register({ context: "component.request.handler.route", name: 5000 },() => {
         throw new Error("Error on port 5000");
     });
 
@@ -26,12 +26,12 @@
     await requestHandler.handle({ host: "localhost", port: 443 });
     await requestHandler.handle({ host: "localhos", port: 6000 });
     
-    const unsecureRequest = await component.require("component.request.unsecure", {gitUsername:"marchuanv"});
-    await unsecureRequest.send({ host: "localhost", port: 3000, path: "/test", method: "GET", headers: {}, data: "", retryCount: 1  });
-    await unsecureRequest.send({ host: "localhost", port: 4000, path: "/test", method: "GET", headers: {}, data: "", retryCount: 1  });
-    await unsecureRequest.send({ host: "localhost", port: 5000, path: "/test", method: "GET", headers: {}, data: "", retryCount: 1  });
-    await unsecureRequest.send({ host: "localhost", port: 443, path: "/test", method: "GET", headers: {}, data: "", retryCount: 1  });
-    await unsecureRequest.send({ host: "localhost", port: 6000, path: "/test", method: "GET", headers: {}, data: "", retryCount: 1  });
+    const { componentRequestUnsecure } = await com.require("component.request.unsecure", {gitUsername:"marchuanv"});
+    let results = await componentRequestUnsecure.send({ host: "localhost", port: 3000, path: "/test", method: "GET", headers: {}, data: "", retryCount: 1  });
+    results = await componentRequestUnsecure.send({ host: "localhost", port: 4000, path: "/test", method: "GET", headers: {}, data: "", retryCount: 1  });
+    results = await componentRequestUnsecure.send({ host: "localhost", port: 5000, path: "/test", method: "GET", headers: {}, data: "", retryCount: 1  });
+    results = await componentRequestUnsecure.send({ host: "localhost", port: 443, path: "/test", method: "GET", headers: {}, data: "", retryCount: 1  });
+    results = await componentRequestUnsecure.send({ host: "localhost", port: 6000, path: "/test", method: "GET", headers: {}, data: "", retryCount: 1  });
     
 })().catch((err)=>{
     console.error(err);
