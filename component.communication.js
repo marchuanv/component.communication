20076c916d6b30071eb60d6846df90a194048792
marchuanv <marchuanv@gmail.com>
2021-04-17 02:50:39 +0200

    refactoring

--- component.communication.js
+++ component.communication.js
@@ -1,87 +1,4 @@
-const http = require("http");
-const dns = require("dns");
-const utils = require("utils");
-const component = require("component");
-
-component.load(module).then(async ({ requestHandler }) => {
-    const registerHost = async () => {
-        if (requestHandler.lock){
-            setTimeout(async () => {
-                await registerHost();
-            },1000);
-            return;
-        } else {
-            requestHandler.lock = true;
-        
-            const host = http.createServer();
-            host.listen(requestHandler.config);
-            
-            host.on("request", (request, response) => {
-                const id = setInterval( async () => {
-                    if (requestHandler.lock){
-                        return;
-                    }
-                    requestHandler.lock = true;
-                    clearInterval(id);
-
-                    let body = '';
-                    request.on('data', chunk => {
-                        body += chunk.toString();
-                    });
-                    request.on('end', async () => {
-                        await requestHandler.log(`received request for ${request.url}`);
-                        const defaultHeaders = {
-                            "Access-Control-Allow-Origin": "*",
-                            "Access-Control-Expose-Headers": "*",
-                            "Access-Control-Allow-Headers": "*",
-                            "Content-Type": "text/plain"
-                        };
-                        const isPreflight = request.headers["access-control-request-headers"] !== undefined;
-                        if(isPreflight) {
-                            requestHandler.lock = false;
-                            return response.writeHead( 200, "Success", defaultHeaders ).end("");
-                        }
-                        let { message } = await requestHandler.publish({
-                            host: requestHandler.config.host,
-                            port: requestHandler.config.port,
-                            path: request.url,
-                            headers: request.headers,
-                            data: body
-                        });
-                        let { headers, statusCode, statusMessage, text } = message || {};
-                        headers = headers || {};
-                        statusCode = statusCode || 500;
-                        statusMessage = statusMessage || "Internal Server Error";
-                        text = text || "";
-                        delete headers["content-length"];
-                        headers["Content-Length"] = Buffer.byteLength(text);
-                        response.writeHead(statusCode, statusMessage, headers ).end(text);
-                        requestHandler.lock = false;
-                    });
-                },1000);
-            });
-            host.on("error", async (hostError) => {
-                if (newHost.host){
-                    dns.lookup(requestHandler.config.host, async (dnsErr) => {
-                        if (dnsErr){
-                            await requestHandler.log(dnsErr);
-                            return await equestHandler.log(`error hosting on ${requestHandler.config.host}:${requestHandler.config.port}`);
-                        }
-                    });
-                } else {
-                    await requestHandler.log(hostError);
-                    return await requestHandler.log(`error hosting on ${requestHandler.config.host}:${requestHandler.config.port}`);
-                }
-            });
-            host.on("listening", async () => {
-                await requestHandler.log(`listening on ${requestHandler.config.host}:${requestHandler.config.port}`);
-            });
-            requestHandler.lock = false;
-        }
-    };
-    await registerHost();
-});
-
+const Communication = require("./lib/communication.js");
 process.on('SIGTERM', () => {
     console.info('SIGTERM signal received.');
     console.log('Closing http server.');
@@ -89,3 +6,4 @@ process.on('SIGTERM', () => {
         console.log('Http server closed.');
     });
 });
+module.exports = { Communication }
