a6ab24224dfb06f0a64f30f58c37610221d194e5
marchuanv <marchuanv@gmail.com>
2021-05-19 12:26:18 +0200

    fix

--- component.communication.js
+++ component.communication.js
@@ -1,9 +1,2 @@
 const { CommunicationIncomingMessage, CommunicationOutgoingMessage, CommunicationResponseMessage, Communication, communicationStatus } = require("./lib/communication.js");
-process.on('SIGTERM', () => {
-    console.info('SIGTERM signal received.');
-    console.log('Closing http server.');
-    host.close(() => {
-        console.log('Http server closed.');
-    });
-});
 module.exports = { CommunicationIncomingMessage, CommunicationOutgoingMessage, CommunicationResponseMessage, Communication, communicationStatus };
