09433d1bb3608986ce09959a4889343eef817d00
marchuanv <marchuanv@gmail.com>
2021-09-06 16:11:02 +0200

    refactor

--- specs.js
+++ specs.js
@@ -2,8 +2,8 @@
     const { Communication, CommunicationRequestHandlerCallback } = require("./initialise.js");
     const comm = new Communication({ host: "localhost", port: 3000 });
     const callback = new CommunicationRequestHandlerCallback({ priority: "High" });
-    callback.handler = () => {
-
+    callback.handle = () => {
+        console.log("handled");
     };
     await comm.handle(callback);
     await comm.start();
