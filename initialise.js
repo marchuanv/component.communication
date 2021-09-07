da515099bd0158d99905c217ecc5992e07c0c824
marchuanv <marchuanv@gmail.com>
2021-09-07 16:51:58 +0200

    refactor

--- initialise.js
+++ initialise.js
@@ -1,6 +1,5 @@
-const { MessageStatus } = require("./lib/messagestatus.js");
 const { Connection } = require("./lib/connection.js");
 const { RequestHandler } = require("./lib/requesthandler.js");
 const { ResponseMessage } = require("./lib/responsemessage.js");
 
-module.exports = { Connection, MessageStatus, RequestHandler, ResponseMessage };
+module.exports = { Connection, RequestHandler, ResponseMessage };
