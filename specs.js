2239692eabaa2f7a44fa2f36924b16a5eb01f46c
marchuanv <marchuanv@gmail.com>
2021-09-07 17:02:59 +0200

    refactor

--- specs.js
+++ specs.js
@@ -1,11 +1,11 @@
 (async() => {
-    const { Connection, RequestHandler } = require("./initialise.js");
+    const { Connection } = require("./initialise.js");
     const connection = new Connection({ host: "localhost", port: 3000, ishttp: true, iswebsocket: false });
     // const callback = new RequestHandler({ Id: "testing", priority: "High", callback: () => {
     //     console.log("handled");
     //     return {};
     // }});
     // await connection.handle(callback);
-    await connection.start();
+    await connection.open();
 })();
 
