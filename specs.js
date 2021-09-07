f57b2bef2356298ce4193fbf9c926ddba1560b6b
marchuanv <marchuanv@gmail.com>
2021-09-07 09:57:54 +0200

    refactor

--- specs.js
+++ specs.js
@@ -3,6 +3,7 @@
     const connection = new Connection({ host: "localhost", port: 3000 });
     const callback = new RequestHandler({ priority: "High", callback: () => {
         console.log("handled");
+        return {};
     }});
     await connection.handle(callback);
     await connection.start();
