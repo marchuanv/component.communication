6ec9d65e3399480ccd5aea49e756cdb6129abc08
marchuanv <marchuanv@gmail.com>
2022-02-07 17:55:54 +0200

    refactoring

--- specs.js
+++ specs.js
@@ -1,6 +1,11 @@
 (async() => {
+
+    await require("./lib/http/httpmessagefactory.spec.js");
+
+
     await require("./lib/message.spec.js");
     await require("./lib/http/httpmessage.spec.js");
     await require("./lib/http/httprequestmessage.spec.js");
+
 })();
 
