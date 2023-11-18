128d24e84efabcbbffe1e9bc28c44548f4dd2ac7
marchuanv <marchuanv@gmail.com>
2023-11-18 22:00:21 +0200

    refactor

--- registry.mjs
+++ registry.mjs
@@ -5,7 +5,7 @@ export { randomUUID } from 'node:crypto';
 export * as url from 'url';
 export { IncomingMessage, ServerResponse, createServer, request } from 'node:http';
 export { Stream } from 'node:stream';
-export { Properties } from 'utils';
+export { Properties } from 'component.properties';
 export { Connection } from './lib/connection.mjs';
 export { ConnectionOptions } from './lib/connection.options.mjs';
 export { ContentType } from './lib/content.type.mjs';
