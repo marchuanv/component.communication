f9715510c6def1ac2160d49c511f53b2e9a1f366
Marchuan Van Der Merwe <marchuanv@sahomeloans.com>
2023-11-21 12:52:53 +0200

    refactor

--- specs/send.multiple.messages.spec.mjs
+++ specs/send.multiple.messages.spec.mjs
@@ -1,5 +1,5 @@
-describe('when creating a connection given successful', () => {
-    it('should send multiple messages and receive multiple messages', async () => {
+describe('when sending multiple messages given a connection was created', () => {
+    it('should send and receive multiple messages', async () => {
         const connection = process.connection;
         const message1Id = 'ca064ae2-dc0c-40ea-ae95-a83934e32bfc';
         const message1ClientData = 'Hello World';
@@ -69,7 +69,6 @@ describe('when creating a connection given successful', () => {
             }).catch((error) => {
                 reject(error);
             });
-
             connection.receive().then(({ clientMessage }) => {
                 count = count + 1;
                 expect(JSON.stringify(clientMessage.body)).toBe(JSON.stringify(expectedServerMessage1.body));
@@ -88,7 +87,6 @@ describe('when creating a connection given successful', () => {
             }).catch((error) => {
                 reject(error);
             });
-
             connection.send(message1);
             connection.send(message2);
         });
