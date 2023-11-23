f4a69991a05f4534d8e2884ba2ed192e574d5cd9
Marchuan Van Der Merwe <marchuanv@sahomeloans.com>
2023-11-23 17:21:23 +0200

    refactor

--- registry.mjs
+++ registry.mjs
@@ -1,17 +1,15 @@
-import dns from 'node:dns';
 import Jasmine from 'jasmine';
-import { Specs } from 'utils'
+import dns from 'node:dns';
+import { Specs } from 'utils';
+export { CtorParam, Properties } from 'component.properties';
 export { EventEmitter } from 'events';
 export { randomUUID } from 'node:crypto';
-export * as url from 'url';
 export { IncomingMessage, ServerResponse, createServer, request } from 'node:http';
 export { Stream } from 'node:stream';
-export { Properties } from 'component.properties';
+export * as url from 'url';
 export { Connection } from './lib/connection.mjs';
 export { ConnectionOptions } from './lib/connection.options.mjs';
 export { ContentType } from './lib/content.type.mjs';
 export { HttpConnection } from './lib/http/http.connection.mjs';
-export { dns };
-export { Jasmine }
-export { Specs }
+export { Jasmine, Specs, dns };
 
