d81e7e8456e5cf76c48a9c8dadbb6e5060336a39
Marchuan Van Der Merwe <marchuanv@sahomeloans.com>
2023-11-24 20:45:31 +0200

    refactor

--- specs/send.multiple.messages.spec.mjs
+++ specs/send.multiple.messages.spec.mjs
@@ -1,6 +1,7 @@
 describe('when sending multiple messages given a connection was created', () => {
     fit('should send and receive multiple messages', async () => {
         const connection = process.connection;
+
         let identifier = 'ca064ae2-dc0c-40ea-ae95-a83934e32bfc';
         const message1ClientData = 'Hello World';
         const message1ServerData = 'message received and is valid';
@@ -8,11 +9,11 @@ describe('when sending multiple messages given a connection was created', () => 
             headers: { identifier },
             data: message1ClientData
         };
-        const expectedClientMessage1 = {
+        const expectedRequestMessage1 = {
             headers: { identifier },
             data: message1ClientData
         };
-        const expectedServerMessage1 = {
+        const expectedResponseMessage1 = {
             headers: { identifier },
             data: message1ServerData
         };
@@ -23,11 +24,11 @@ describe('when sending multiple messages given a connection was created', () => 
             headers: { identifier },
             data: message2ClientData
         };
-        const expectedClientMessage2 = {
+        const expectedRequestMessage2 = {
             headers: { identifier },
             data: message2ClientData
         };
-        const expectedServerMessage2 = {
+        const expectedResponseMessage2 = {
             headers: { identifier },
             data: message2ServerData
         };
@@ -35,28 +36,39 @@ describe('when sending multiple messages given a connection was created', () => 
         const promise = new Promise((resolve, reject) => {
             connection.receive().then((message) => {
                 count = count + 1;
-                expect(JSON.stringify(message.data)).toBe(JSON.stringify(expectedClientMessage1.data));
-                if (count === 4) {
-                    resolve();
+                const { requestid, responseid } = message.headers;
+                const isClientSide = (requestid === responseid) ? true: false;
+                if (isClientSide) {
+                    throw new Error('expected server message');
                 }
+                expect(JSON.stringify(message.data)).toBe(JSON.stringify(expectedRequestMessage1.data));
             }).catch(reject);
             connection.receive().then((message) => {
                 count = count + 1;
-                expect(JSON.stringify(message.data)).toBe(JSON.stringify(expectedClientMessage2.data));
-                if (count === 4) {
-                    resolve();
+                const { requestid, responseid } = message.headers;
+                const isClientSide = (requestid === responseid) ? true: false;
+                if (isClientSide) {
+                    throw new Error('expected server message');
                 }
+                expect(JSON.stringify(message.data)).toBe(JSON.stringify(expectedRequestMessage2.data));
             }).catch(reject);
             connection.receive().then((message) => {
                 count = count + 1;
-                expect(JSON.stringify(message.data)).toBe(JSON.stringify(expectedServerMessage1.data));
-                if (count === 4) {
-                    resolve();
+                const { requestid, responseid } = message.headers;
+                const isClientSide = (requestid === responseid) ? true: false;
+                if (!isClientSide) {
+                    throw new Error('expected client message');
                 }
+                expect(JSON.stringify(message.data)).toBe(JSON.stringify(expectedResponseMessage1.data));
             }).catch(reject);
             connection.receive().then((message) => {
                 count = count + 1;
-                expect(JSON.stringify(message.data)).toBe(JSON.stringify(expectedServerMessage2.data));
+                const { requestid, responseid } = message.headers;
+                const isClientSide = (requestid === responseid) ? true: false;
+                if (!isClientSide) {
+                    throw new Error('expected client message');
+                }
+                expect(JSON.stringify(message.data)).toBe(JSON.stringify(expectedResponseMessage2.data));
                 if (count === 4) {
                     resolve();
                 }
