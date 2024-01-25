2f1026bcda9b6763c4dca80c191a8a79e74dcfac
Marchuan Van Der Merwe <marchuanv@sahomeloans.com>
2024-01-25 18:02:51 +0200

    refactor

--- registry.mjs
+++ registry.mjs
@@ -9,6 +9,8 @@ export { Connection, ConnectionCtorArgs } from "./lib/connection.mjs";
 export { ConnectionOptions, ConnectionOptionsCtorArgs } from "./lib/connection.options.mjs";
 export { ContentType } from './lib/content.type.mjs';
 export { HttpConnection, HttpConnectionCtorArgs } from "./lib/http/http.connection.mjs";
+export { MessageHeaders } from './lib/message.headers.mjs';
 export { Message, MessageCtorArgs } from "./lib/message.mjs";
+export { MessagePriority } from './lib/message.priority.mjs';
 export { GUID, Specs, dns };
 
--- specs/send.multiple.messages.spec.mjs
+++ specs/send.multiple.messages.spec.mjs
@@ -1,44 +1,8 @@
-import { Connection, ConnectionCtorArgs, ConnectionOptions, ConnectionOptionsCtorArgs, HttpConnection, HttpConnectionCtorArgs, Message, MessageCtorArgs } from "../registry.mjs";
+import { MessageHeadersCtorArgs } from "../lib/message.headers.mjs";
+import { Connection, ConnectionCtorArgs, ConnectionOptions, ConnectionOptionsCtorArgs, HttpConnection, HttpConnectionCtorArgs, Message, MessageCtorArgs, MessageHeaders, MessagePriority } from "../registry.mjs";
 
 describe('when sending multiple messages given a connection was created', () => {
     it('should send and receive multiple messages', async () => {
-
-        let identifier = 'ca064ae2-dc0c-40ea-ae95-a83934e32bfc';
-        const message1ClientData = 'Hello World';
-        const message1ServerData = 'message received and is valid';
-
-        const message1CtorArgs = new MessageCtorArgs();
-        message1CtorArgs.data = message1ClientData;
-
-        const message1 = new Message(message1CtorArgs);
-        message1.headers = { identifier };
-
-        const expectedRequestMessage1 = {
-            headers: { identifier },
-            data: message1ClientData
-        };
-        const expectedResponseMessage1 = {
-            headers: { identifier },
-            data: message1ServerData
-        };
-        identifier = '6c0e1b03-ca39-483f-9e93-3c6077626dda';
-        const message2ClientData = 'Hello World Again';
-        const message2ServerData = 'message received and is valid';
-
-        const message2CtorArgs = new MessageCtorArgs();
-        message2CtorArgs.data = message2ClientData;
-        const message2 = new Message(message2CtorArgs);
-        message2.headers = { identifier };
-
-        const expectedRequestMessage2 = {
-            headers: { identifier },
-            data: message2ClientData
-        };
-        const expectedResponseMessage2 = {
-            headers: { identifier },
-            data: message2ServerData
-        };
-        let count = 0;
 
         const connectionOptionsArgs = new ConnectionOptionsCtorArgs();
         connectionOptionsArgs.maxRetryCount = 3;
@@ -47,6 +11,66 @@ describe('when sending multiple messages given a connection was created', () => 
         connectionOptionsArgs.hostPort = 8080;
         connectionOptionsArgs.remoteHostName = 'localhost';
         connectionOptionsArgs.remoteHostPort = 8080;
+
+        const message1ClientData = 'Hello World';
+        const message1ServerData = 'message received and is valid';
+
+        const message1HeadersCtorArgs = new MessageHeadersCtorArgs();
+        message1HeadersCtorArgs.clienthost = connectionOptionsArgs.hostName;
+        message1HeadersCtorArgs.clientport = connectionOptionsArgs.hostPort;
+        message1HeadersCtorArgs.priority = MessagePriority.High;
+        message1HeadersCtorArgs.time = 10;
+        const message1Headers = new MessageHeaders(message1HeadersCtorArgs);
+
+        const message1CtorArgs = new MessageCtorArgs();
+        message1CtorArgs.clienthost = connectionOptionsArgs.hostName;
+        message1CtorArgs.clientport = connectionOptionsArgs.hostPort;
+        message1CtorArgs.headers = message1Headers;
+        message1CtorArgs.priority = message1Headers.priority;
+        message1CtorArgs.data = message1ClientData;
+        message1CtorArgs.time = message1HeadersCtorArgs.time;
+
+        const message1 = new Message(message1CtorArgs);
+
+        const expectedRequestMessage1 = {
+            headers: { identifier: message1.headers.identifier },
+            data: message1ClientData
+        };
+        const expectedResponseMessage1 = {
+            headers: { identifier: message1.headers.identifier },
+            data: message1ServerData
+        };
+
+        const message2ClientData = 'Hello World Again';
+        const message2ServerData = 'message received and is valid';
+
+        const message2HeadersCtorArgs = new MessageHeadersCtorArgs();
+        message2HeadersCtorArgs.clienthost = connectionOptionsArgs.hostName;
+        message2HeadersCtorArgs.clientport = connectionOptionsArgs.hostPort;
+        message2HeadersCtorArgs.priority = MessagePriority.Medium;
+        message2HeadersCtorArgs.time = 20;
+        const message2Headers = new MessageHeaders(message2HeadersCtorArgs);
+
+        const message2CtorArgs = new MessageCtorArgs();
+        message2CtorArgs.clienthost = connectionOptionsArgs.hostName;
+        message2CtorArgs.clientport = connectionOptionsArgs.hostPort;
+        message2CtorArgs.headers = message2Headers;
+        message2CtorArgs.priority = message2Headers.priority;
+        message2CtorArgs.data = message2ClientData;
+        message2CtorArgs.time = message2HeadersCtorArgs.time;
+
+        const message2 = new Message(message2CtorArgs);
+
+        const expectedRequestMessage2 = {
+            headers: { identifier: message2.headers.identifier },
+            data: message2ClientData
+        };
+        const expectedResponseMessage2 = {
+            headers: { identifier: message2.headers.identifier },
+            data: message2ServerData
+        };
+        let count = 0;
+
         const connectionOptions = new ConnectionOptions(connectionOptionsArgs);
 
         const httpConnectionArgs = new HttpConnectionCtorArgs();
